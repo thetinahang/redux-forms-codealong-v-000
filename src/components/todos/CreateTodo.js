a42ce1e7c35cea7beeaac4103f6dc302ffc445f4
Tina Hang <mstinahang@gmail.com>
2018-11-13 04:30:52 +0000

    automatically backed up by learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
 
 class CreateTodo extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+   
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
   render(){
     return(
       <div>
